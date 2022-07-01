e20eb31b0901f8127798cddd3797b6116f832756
sonjungmin <jm981001@naver.com>
2022-07-01 21:02:40 +0900

    init commit

---
+++ src/Ex2.java
@@ -0,0 +1,33 @@
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(' ');
+		System.out.println('아');
+		System.out.println("아이티윌");
+//		System.out.println('아이티윌'); //오류
+		System.out.println("아");
+//		System.out.println(""); //공백 없어도 됨
+		
+		//문자의 연산
+		System.out.println("아이"+"티윌");
+		System.out.println(1+1);
+		System.out.println("1"+"1");
+
+		//선생님이 말했다 "아! 자바 재밌다" 출력하기
+		System.out.println("선생님이 말했다 \"아! 자바 재밌다\""); // \가 있으면 문자로 판단해라
+		System.out.println("선생님이 말했다");
+		System.out.println("\"아! 자바 너무 재밌다!\"");
+		System.out.println("선생님이 말했다\n\"아! 자바 재밌다\"");
+		
+		System.out.print("아이");
+		System.out.print("티윌");
+		System.out.println();
+		
+		System.out.println("영어\t국어\t수학");
+		System.out.println("90\t80\t70");
+		
+	}
+
+}
